dd7c4f6a6d85ac8a4b07fdf44231e922911d87f3
Rossella Mascia <rossella.mascia@gmail.com>
2022-02-26 13:28:04 +0100

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,13 @@ function MyApp({ Component, pageProps }: AppProps) {
           strategy="lazyOnload"
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
         />
-
+        <Script id="googlegtag">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PVWKHDT');`}
+        </Script>
         <Script strategy="lazyOnload" id="gtag">
           {`
           window.dataLayer = window.dataLayer || [];
