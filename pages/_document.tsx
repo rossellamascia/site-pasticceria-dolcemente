d783d9d1ccbdf289e56402792e86806b6c55bd50
Rossella Maascia <rossella.mascia@gmail.com>
2022-03-31 18:17:15 +0200

    fix analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,26 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="it">
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="vYZSy_j5Dch2UH7ZSvRb6_gEX59vKLNXGIQUNncucEE"
-        />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          property="og:title"
-          content="Dolcemente Pasticceria"
-          key="title"
-        />
-        <meta
-          name="description"
-          content="Pasticceria e caffetteria marina di carrara in toscana, colazione, pasticcini, torte per compleanni, matrimoni e anniversari"
-        />
-        <meta
-          property="og:image"
-          content="https://www.pasticceriadolcemente.it/images/pasticceria-dolcemente.jpeg"
-        />
-      </Head>
+      <Head />
       <body>
         <noscript>
           <iframe
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -43,6 +43,16 @@ export const Home: NextPage = () => {
           name="description"
           content="Pasticceria e caffetteria marina di carrara in toscana, colazione, pasticcini, torte per compleanni, matrimoni e anniversari"
         />
+
+        <meta
+          name="google-site-verification"
+          content="vYZSy_j5Dch2UH7ZSvRb6_gEX59vKLNXGIQUNncucEE"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          property="og:image"
+          content="https://www.pasticceriadolcemente.it/images/pasticceria-dolcemente.jpeg"
+        />
       </Head>
       <Hero />
       <Card data={cardData} />
